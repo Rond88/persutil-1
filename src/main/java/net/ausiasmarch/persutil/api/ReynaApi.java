a0bc9cd22a9be530081b80d52e7ed257eb6115da
rond88 <r0nald19202006@gmail.com>
2025-11-25 09:48:15 +0100

    Implement CRUD operations and pagination for ReynaEntity in ReynaApi and ReynaService

--- src/main/java/net/ausiasmarch/persutil/api/ReynaApi.java
+++ src/main/java/net/ausiasmarch/persutil/api/ReynaApi.java
@@ -1,5 +1,64 @@
 package net.ausiasmarch.persutil.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.ausiasmarch.persutil.entity.ReynaEntity;
+import net.ausiasmarch.persutil.service.ReynaService;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/reyna")
 public class ReynaApi {
-    
+
+    @Autowired
+    ReynaService oReynaService;
+
+    //prueba QUITAR
+    @GetMapping("/hola")
+    public ResponseEntity<String> hola() {
+        return new ResponseEntity<>("\"Hola desde el blog\"", HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ReynaEntity> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(oReynaService.get(id));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Long> create(@RequestBody ReynaEntity oReynaEntity) {
+        return ResponseEntity.ok(oReynaService.create(oReynaEntity));
+    }
+
+    @PutMapping("")
+    public ResponseEntity<Long> update(@RequestBody ReynaEntity oReynaEntity) {
+        return ResponseEntity.ok(oReynaService.update(oReynaEntity));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Long> delete(@PathVariable Long id) {
+        return ResponseEntity.ok(oReynaService.delete(id));
+    }
+
+    @GetMapping("")
+    public ResponseEntity<Page<ReynaEntity>> getPage(Pageable oPageable) {
+        return ResponseEntity.ok(oReynaService.getPage(oPageable));
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<Long> count() {
+        return ResponseEntity.ok(oReynaService.count());
+    }
 }
--- src/main/java/net/ausiasmarch/persutil/service/ReynaService.java
+++ src/main/java/net/ausiasmarch/persutil/service/ReynaService.java
@@ -1,12 +1,52 @@
 package net.ausiasmarch.persutil.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import net.ausiasmarch.persutil.entity.ReynaEntity;
 import net.ausiasmarch.persutil.repository.ReynaRepository;
 
 @Service
 public class ReynaService {
     @Autowired
     ReynaRepository oReynaRepository;
+
+    public ReynaEntity get(Long id) {
+        return oReynaRepository.findById(id).orElse(null);
+    }
+
+    public Long create(ReynaEntity oReynaEntity) {
+        oReynaEntity.setFechaCreacion(LocalDateTime.now());
+        oReynaEntity.setFechaModificacion(null);
+        oReynaRepository.save(oReynaEntity);
+        return oReynaEntity.getId();
+    }
+
+    public Long update(ReynaEntity oReynaEntity) {
+        ReynaEntity existingReyna= oReynaRepository.findById(oReynaEntity.getId())
+                .orElseThrow(() -> new RuntimeException("Reyna not found"));
+        existingReyna.setFrase(oReynaEntity.getFrase());
+        existingReyna.setAutor(oReynaEntity.getAutor());
+        existingReyna.setEsPublica(oReynaEntity.isEsPublica());
+        existingReyna.setFechaModificacion(LocalDateTime.now());
+        oReynaRepository.save(existingReyna);
+        return existingReyna.getId();
+    }
+
+    public Long delete(Long id) {
+        oReynaRepository.deleteById(id);
+        return id;
+    }
+
+    public Page<ReynaEntity> getPage(Pageable oPageable) {
+        return oReynaRepository.findAll(oPageable);
+    }
+
+    public Long count() {
+        return oReynaRepository.count();
+    }
 }
